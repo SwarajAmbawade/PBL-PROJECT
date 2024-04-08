44b5ba93d1f2a3a1f55892e6be905fbff8e45e4b
Sahil Adit <aditsahil17@gmail.com>
2024-04-08 06:09:25 +0530

    new files added

---
+++ app.js
@@ -0,0 +1,9 @@
+let btn = document.getElementById("user-menu-button")
+let dropdown = document.querySelector("#dropdown")
+btn.addEventListener('click',function(){
+    btn.style.display = "";
+})
+
+function click(){
+    
+}
